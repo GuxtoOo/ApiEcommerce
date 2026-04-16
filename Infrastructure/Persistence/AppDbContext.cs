91483d5f1331b0511605046d63a43103cd418d46
augusto.santos <augusto.santos@fandi.com.br>
2026-04-16 15:49:10 -0300

    refactor: aperfeiçoamento da conexão com o banco de dados

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -11,18 +11,30 @@ public class AppDbContext : DbContext
     {
         b.Entity<Order>(o =>
         {
+            o.ToTable("Orders");
+
             o.HasKey(x => x.Id);
+
+            o.Property(x => x.Status)
+                .HasConversion<int>();
+
+            o.HasIndex(x => x.BuyerId);
+            o.HasIndex(x => x.Status);
+
             o.OwnsMany(x => x.itens, i =>
-                {
-                    i.WithOwner().HasForeignKey("OrderId");
+            {
+                i.ToTable("OrderItems");
 
-                    i.Property(p => p.ProductId).IsRequired();
-                    i.Property(p => p.Price).HasColumnType("decimal(18,2)");
-                    i.Property(p => p.Quantity).IsRequired();
-                    i.Property(p => p.TotalPrice).HasColumnType("decimal(18,2)");
+                i.WithOwner().HasForeignKey("OrderId");
 
-                    i.HasKey("Id");
-                });
+                i.Property<int>("Id");
+                i.HasKey("Id");
+
+                i.Property(p => p.ProductId).IsRequired();
+                i.Property(p => p.Price).HasColumnType("decimal(18,2)");
+                i.Property(p => p.Quantity).IsRequired();
+                i.Property(p => p.TotalPrice).HasColumnType("decimal(18,2)");
+            });
         });
     }
 }
