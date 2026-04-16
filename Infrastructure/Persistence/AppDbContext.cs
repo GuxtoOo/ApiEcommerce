da546041adc1ada40f11ae271f669f872cf5c19f
augusto.santos <augusto.santos@fandi.com.br>
2026-04-16 11:31:14 -0300

    fix: adição dos itens do pedido em sua devida tabela no envio ao banco de dados

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -8,5 +8,21 @@ public class AppDbContext : DbContext
     public DbSet<Order> Orders => Set<Order>();
     public AppDbContext(DbContextOptions<AppDbContext> o) : base(o) { }
     protected override void OnModelCreating(ModelBuilder b)
-    { b.Entity<Order>(o => { o.HasKey(x => x.Id); o.OwnsMany(x => x.Items); }); }
+    {
+        b.Entity<Order>(o =>
+        {
+            o.HasKey(x => x.Id);
+            o.OwnsMany(x => x.itens, i =>
+                {
+                    i.WithOwner().HasForeignKey("OrderId");
+
+                    i.Property(p => p.ProductId).IsRequired();
+                    i.Property(p => p.Price).HasColumnType("decimal(18,2)");
+                    i.Property(p => p.Quantity).IsRequired();
+                    i.Property(p => p.TotalPrice).HasColumnType("decimal(18,2)");
+
+                    i.HasKey("Id");
+                });
+        });
+    }
 }
